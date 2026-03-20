eb07ade11c684a8c0052ff18f09971778c95fd84
escannorrr <nishadkhadilkar81@gmail.com>
2026-03-20 19:45:05 +0530

    read me file added

--- lib/core/config/env_config.dart
+++ lib/core/config/env_config.dart
@@ -1,4 +1,4 @@
 class EnvConfig {
-  static const String baseUrl = 'http://localhost:8000/api/v1';
+  static const String baseUrl = 'http://127.0.0.1:8000/api/v1';
   static const bool isProduction = false;
 }
